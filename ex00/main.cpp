becc2b7893d64a1d6f43f54aa0a25ae5e36dce2b
sdxsd <willmaguire@icloud.com>
2023-08-09 01:56:25 +0200

    Code.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,6 +6,7 @@ int main(void) {
 	std::vector<int> x = { 1, 2, 3, 4, 5 };
 
 	try {
+		std::cout << *easyfind(x, 3) << std::endl;
 		std::cout << *easyfind(x, 0) << std::endl;
 	}
 	catch (std::invalid_argument &ia) {
