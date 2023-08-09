df729363ef1ca82be497fa3c848cc87788a4f4fc
sdxsd <willmaguire@icloud.com>
2023-08-09 02:24:16 +0200

    Span class implementation.

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -1,1 +1,32 @@
 #include "Span.hpp"
+
+Span::Span(unsigned int n) {
+	maxInteger = n;
+}
+
+Span::Span(const Span& toCopy) {
+	maxInteger = toCopy.maxInteger;
+	std::vector<int>::const_iterator iter = toCopy.intList.begin();
+	for (; iter < toCopy.intList.end(); iter++)
+		intList.push_back(*iter);
+}
+
+Span::~Span(void) {
+	intList.clear();
+}
+
+Span& Span::operator=(const Span& toCopy) {
+	;
+}
+
+unsigned int Span::shortestSpan(void) {
+	;
+}
+
+unsigned int Span::longestSpan(void) {
+	;
+}
+
+void Span::addNumber(int n) {
+	;
+}
