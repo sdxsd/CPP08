c2c2a9e9827dccc7da42424d2101e1cf6c5e56d5
sdxsd <willmaguire@icloud.com>
2023-08-09 02:12:15 +0200

    Span class.

--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -1,6 +1,20 @@
 #ifndef SPAN_H
 #define SPAN_H
 
+#include <vector>
 
+class Span {
+	private:
+		unsigned int		maxInteger;
+		std::vector<int>	intList;
+	public:
+		Span(unsigned int n);
+		Span(const Span& toCopy);
+		~Span(void);
+		Span& operator=(const Span& toCopy);
+		unsigned int shortestSpan(void);
+		unsigned int longestSpan(void);
+		void addNumber(int n);
+};
 
 #endif // SPAN_H
